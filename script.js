96feb4be6c4d832b6e5692eaeb64dd466bd7d683
novabytecretion-png <novabytecretion@gmail.com>
2026-01-02 23:23:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC5I86lYBkA5A4BHx9IiobotDFL2_L_TF8",
+  authDomain: "editsync-56003.firebaseapp.com",
+  projectId: "editsync-56003",
+  storageBucket: "editsync-56003.firebasestorage.app",
+  messagingSenderId: "1028057363510",
+  appId: "1:1028057363510:web:ea02abf953b9b35d30d497"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
